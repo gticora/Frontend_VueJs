9cc590267b13ad84b0315fb50dadb65157dcf669
Gustavo Ticora <itia_e_gticora@inteia.com.co>
2024-11-12 00:19:20 -0500

    ajuste puerto de escucha

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    host: '0.0.0.0',                 // Escucha en todas las interfaces de red
+    port: process.env.PORT || 8080,  // Usa el puerto proporcionado por Railway o el 8080 como respaldo
+  },
+};
